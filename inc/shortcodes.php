72cbe91272a17280464dc05f560fec8a99242038
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 04:22:33 +0700

    Harden button shortcode output

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -13,11 +13,21 @@ function hovercraft_button_shortcode( $atts, $content = null ) {
 		'button'
 	);
 
+	$allowed_targets = array( '_self', '_blank', '_parent', '_top' );
+	$target = in_array( $atts['target'], $allowed_targets, true ) ? $atts['target'] : '_self';
 	$style_class = ( $atts['style'] === 'secondary' ) ? 'button-secondary' : 'button-primary';
-	$rel_attr = $atts['rel'] ? ' rel="' . esc_attr( $atts['rel'] ) . '"' : '';
-	$class_attr = trim( $style_class . ' ' . $atts['class'] );
+	$custom_class = sanitize_html_class( $atts['class'] );
+	$class_attr = trim( $style_class . ' ' . $custom_class );
+	$rel = sanitize_text_field( $atts['rel'] );
 
-	return '<a href="' . esc_url( $atts['url'] ) . '" target="' . esc_attr( $atts['target'] ) . '" class="' . esc_attr( $class_attr ) . '"' . $rel_attr . '>' . do_shortcode( $content ) . '</a>';
+	if ( '_blank' === $target && empty( $rel ) ) {
+		$rel = 'noopener noreferrer';
+	}
+
+	$rel_attr = $rel ? ' rel="' . esc_attr( $rel ) . '"' : '';
+	$button_content = wp_kses_post( do_shortcode( $content ) );
+
+	return '<a href="' . esc_url( $atts['url'] ) . '" target="' . esc_attr( $target ) . '" class="' . esc_attr( $class_attr ) . '"' . $rel_attr . '>' . $button_content . '</a>';
 }
 add_shortcode( 'button', 'hovercraft_button_shortcode' );
 
