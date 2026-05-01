0764b29c62352c350e0a0311fae6ccba6551004f
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 04:32:32 +0700

    Harden archive template output

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 	$hovercraft_sidebar_appears = get_theme_mod( 'hovercraft_sidebar_appears', 'everywhere' );
 	$hovercraft_primary_width = get_theme_mod( 'hovercraft_primary_width', 'wide' );
 
-	if ( $hovercraft_sidebar_appears == 'everywhere' ) { ?><div id="primary"><?php } 
-	elseif ( $hovercraft_sidebar_appears == 'posts_only' || $hovercraft_sidebar_appears == 'none' ) {
-		if ( $hovercraft_primary_width == 'narrow_centered' ) { ?>
+	if ( $hovercraft_sidebar_appears === 'everywhere' ) { ?><div id="primary"><?php } 
+	elseif ( $hovercraft_sidebar_appears === 'posts_only' || $hovercraft_sidebar_appears === 'none' ) {
+		if ( $hovercraft_primary_width === 'narrow_centered' ) { ?>
 			<div id="primary-center">
 		<?php } 
-		elseif ( $hovercraft_primary_width == 'wide' ) { ?>
+		elseif ( $hovercraft_primary_width === 'wide' ) { ?>
 			<div id="primary-wide">
 		<?php } 
 	} ?>
@@ -34,7 +34,7 @@
         		// the query
 				// https://wordpress.stackexchange.com/questions/145125/display-content-from-a-specific-category
 				// https://wordpress.stackexchange.com/questions/14768/determine-if-page-is-the-posts-page
-				if ( $blog_category != 'none' ) {
+				if ( $blog_category !== 'none' ) {
 					// https://wordpress.stackexchange.com/questions/133754/pagination-shows-same-contents-all-pages
         			$the_query = new WP_Query(array(
             			'category_name' => $blog_category,
@@ -62,10 +62,10 @@
 					
 				<?php 
 				$hovercraft_primary_width = get_theme_mod( 'hovercraft_primary_width', 'wide' );
-				if ( $hovercraft_primary_width == 'wide' ) { get_template_part( 'template-parts/content/featured-image-archive-large' ); } 
+				if ( $hovercraft_primary_width === 'wide' ) { get_template_part( 'template-parts/content/featured-image-archive-large' ); } 
 				else { get_template_part( 'template-parts/content/featured-image-archive' ); } ?>
 				
-				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+				<h4><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4>
 
 				<?php get_template_part( 'template-parts/content/byline-archive' ); ?>
 		
@@ -79,7 +79,7 @@
 				
 				<h1><?php the_archive_title(); ?></h1>
 				
-				<div class="category-description"><?php echo category_description(); ?></div> 
+				<div class="category-description"><?php echo wp_kses_post( category_description() ); ?></div> 
 				
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -88,10 +88,10 @@
 					
 				<?php 
 				$hovercraft_primary_width = get_theme_mod( 'hovercraft_primary_width', 'wide' );
-				if ( $hovercraft_primary_width == 'wide' ) { get_template_part( 'template-parts/content/featured-image-archive-large' ); } 
+				if ( $hovercraft_primary_width === 'wide' ) { get_template_part( 'template-parts/content/featured-image-archive-large' ); } 
 				else { get_template_part( 'template-parts/content/featured-image-archive' ); } ?>
 				
-				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+				<h4><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4>
 
 				<?php get_template_part( 'template-parts/content/byline-archive' ); ?>
 		
@@ -112,7 +112,7 @@
   	</div><!-- primary -->
   
     <?php $hovercraft_sidebar_appears = get_theme_mod( 'hovercraft_sidebar_appears', 'everywhere' );
-		if ( $hovercraft_sidebar_appears == 'everywhere' ) { 
+		if ( $hovercraft_sidebar_appears === 'everywhere' ) { 
 			get_template_part( 'sidebar' ); 
 		} ?>
     
