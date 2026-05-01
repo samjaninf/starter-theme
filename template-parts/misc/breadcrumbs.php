1a9a3fc6e02e8c9a10d216295ce4399ef852c154
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 05:12:46 +0700

    Clean breadcrumbs display checks

--- template-parts/misc/breadcrumbs.php
+++ template-parts/misc/breadcrumbs.php
@@ -1,15 +1,15 @@
 <?php
 $hovercraft_breadcrumbs = get_theme_mod( 'hovercraft_breadcrumbs', 'sitewide_except_homepage' );
 
-if ( ($hovercraft_breadcrumbs == 'sitewide') || ($hovercraft_breadcrumbs == 'sitewide_except_homepage') ) {
+if ( $hovercraft_breadcrumbs === 'sitewide' || $hovercraft_breadcrumbs === 'sitewide_except_homepage' ) {
 	
-	if ( ($hovercraft_breadcrumbs == 'sitewide_except_homepage') && (!is_front_page()) ) { ?>
+	if ( $hovercraft_breadcrumbs === 'sitewide_except_homepage' && ! is_front_page() ) { ?>
 		
 		<div class="breadcrumbs">
     		<?php ah_breadcrumb(); ?>
 		</div><!-- breadcrumbs -->
 		
-	<?php } elseif ($hovercraft_breadcrumbs == 'sitewide') { ?>
+	<?php } elseif ( $hovercraft_breadcrumbs === 'sitewide' ) { ?>
 		
 		<div class="breadcrumbs">
     		<?php ah_breadcrumb(); ?>
