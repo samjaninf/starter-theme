093ffbd2bca1a2151a884fbd030b82f80edfa345
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 05:10:08 +0700

    Clean archive last modified output

--- template-parts/content/archive/last-modified.php
+++ template-parts/content/archive/last-modified.php
@@ -1,3 +1,3 @@
 <div class="last-modified">
-    <span><?php _e( 'Last modified on ', 'hovercraft' ); echo the_modified_time('F j, Y'); ?></span>
+    <span><?php esc_html_e( 'Last modified on ', 'hovercraft' ); echo esc_html( get_the_modified_time( 'F j, Y' ) ); ?></span>
 </div><!-- last-modified -->
