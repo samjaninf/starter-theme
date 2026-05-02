fc5760e0e0f2175160f1edfad18959472ffbe7d4
Jesse Nickles <jesse@littlebizzy.com>
2026-05-03 02:43:18 +0700

    Use strict comparisons in single template

--- single.php
+++ single.php
@@ -10,10 +10,10 @@
 		$hovercraft_sidebar_appears = get_theme_mod( 'hovercraft_sidebar_appears', 'everywhere' );
 		$hovercraft_primary_width = get_theme_mod( 'hovercraft_primary_width', 'narrow_centered' );
 
-		if ( $hovercraft_sidebar_appears == 'everywhere' || $hovercraft_sidebar_appears == 'posts_only' ) { ?><div id="primary"><?php } 
-		elseif ( $hovercraft_sidebar_appears == 'none' ) {
-			if ( $hovercraft_primary_width == 'narrow_centered' ) { ?><div id="primary-center"><?php } 
-			elseif ( $hovercraft_primary_width == 'wide' ) { ?><div id="primary-wide"><?php } 
+		if ( $hovercraft_sidebar_appears === 'everywhere' || $hovercraft_sidebar_appears === 'posts_only' ) { ?><div id="primary"><?php } 
+		elseif ( $hovercraft_sidebar_appears === 'none' ) {
+			if ( $hovercraft_primary_width === 'narrow_centered' ) { ?><div id="primary-center"><?php } 
+			eelseif ( $hovercraft_primary_width === 'wide' ) { ?><div id="primary-wide"><?php } 
 		} ?>
 		
 		<div id="content-wrapper">
@@ -21,7 +21,7 @@
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 				<?php $hovercraft_featured_image_position = get_theme_mod( 'hovercraft_featured_image_position', 'above_title' );
-					if ( $hovercraft_featured_image_position == 'above_title' ) { ?>
+					if ( $hovercraft_featured_image_position === 'above_title' ) { ?>
 			
 					<?php get_template_part( 'template-parts/content/featured-image' ); ?>
 			
@@ -34,7 +34,7 @@
 					<?php get_template_part( 'template-parts/content/byline' ); ?>
 
 					<?php $hovercraft_social_sharing = get_theme_mod( 'hovercraft_social_sharing', 'bottom_of_post' );
-					if ( $hovercraft_social_sharing == 'top_of_post' || $hovercraft_social_sharing == 'top_and_bottom_of_post' ) {
+					if ( $hovercraft_social_sharing === 'top_of_post' || $hovercraft_social_sharing === 'top_and_bottom_of_post' ) {
 						get_template_part( 'template-parts/content/social-sharing' ); 
 					} ?>
 
@@ -51,7 +51,7 @@
 					<?php get_template_part( 'template-parts/content/byline' ); ?>
 
 					<?php $hovercraft_social_sharing = get_theme_mod( 'hovercraft_social_sharing', 'bottom_of_post' );
-					if ( $hovercraft_social_sharing == 'top_of_post' || $hovercraft_social_sharing == 'top_and_bottom_of_post' ) {
+					if ( $hovercraft_social_sharing === 'top_of_post' || $hovercraft_social_sharing === 'top_and_bottom_of_post' ) {
 						get_template_part( 'template-parts/content/social-sharing' ); 
 					} ?>
 
@@ -68,7 +68,7 @@
 					<?php get_template_part( 'template-parts/content/loop-after' ); ?>
 
 					<?php $hovercraft_social_sharing = get_theme_mod( 'hovercraft_social_sharing', 'bottom_of_post' );
-					if ( $hovercraft_social_sharing == 'bottom_of_post' || $hovercraft_social_sharing == 'top_and_bottom_of_post' ) {
+					if ( $hovercraft_social_sharing === 'bottom_of_post' || $hovercraft_social_sharing === 'top_and_bottom_of_post' ) {
 						get_template_part( 'template-parts/content/social-sharing' ); 
 					} ?>
 
@@ -94,7 +94,7 @@
 	</div><!-- primary -->
 
 		<?php $hovercraft_sidebar_appears = get_theme_mod( 'hovercraft_sidebar_appears', 'everywhere' );
-		if ( $hovercraft_sidebar_appears == 'everywhere' || $hovercraft_sidebar_appears == 'posts_only' ) { 
+		if ( $hovercraft_sidebar_appears === 'everywhere' || $hovercraft_sidebar_appears === 'posts_only' ) { 
 			get_template_part( 'sidebar' ); 
 		} ?>
 		
