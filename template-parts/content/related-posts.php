b4dc5789a54416cf985a08fb20c476030ce7673b
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 17:26:47 +0700

    Escape frontend template text output

--- template-parts/content/related-posts.php
+++ template-parts/content/related-posts.php
@@ -15,7 +15,7 @@ $args = [
 $my_query = new wp_query( $args );
 if( $my_query->have_posts() ) { ?>
     <div id="related">
-        <h4><?php _e( 'Related Posts: ', 'hovercraft' ); ?></h4>
+        <h4><?php esc_html_e( 'Related Posts: ', 'hovercraft' ); ?></h4>
         <ul><?php while( $my_query->have_posts() ) { $my_query->the_post(); ?><li><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></li><?php } wp_reset_postdata(); ?></ul>
     </div><!-- related -->
 <?php }
