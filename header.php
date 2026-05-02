8dc752d78b83a2454ff169a18686db0f76e32994
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 16:09:51 +0700

    Clean header charset and icon URL output

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
-        <meta charset="utf-8">
+        <meta charset="<?php echo esc_attr( get_bloginfo( 'charset' ) ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
@@ -21,7 +21,8 @@
 
         // output material icons if not 'none' and valid
         if ( $material_icons_setting !== 'none' && isset( $material_icons_map[ $material_icons_setting ] ) ) {
-        ?><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=<?php echo esc_attr( $material_icons_map[ $material_icons_setting ] ); ?>&display=block"><?php 
+            $material_icons_url = 'https://fonts.googleapis.com/css2?family=' . $material_icons_map[ $material_icons_setting ] . '&display=block';
+        ?><link rel="stylesheet" href="<?php echo esc_url( $material_icons_url ); ?>"><?php 
         } ?>
 
         <?php 
