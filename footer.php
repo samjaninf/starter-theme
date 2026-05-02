1b7684ce06bbd766cb75938f0102f3f82c597565
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 14:30:53 +0700

    Clean search icon family handling

--- footer.php
+++ footer.php
@@ -136,14 +136,13 @@ if ( $hovercraft_copyright_width === 'full' ) {
 			<input type="search" class="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" placeholder="<?php esc_attr_e( 'Search entire website... ', 'hovercraft' ); ?>" />
 			<button type="submit" class="search-submit" name="submit">
 				<?php
-				$hovercraft_layout_icons = get_theme_mod( 'hovercraft_layout_icons', 'material_icons_classic' );
 				if ( $hovercraft_layout_icons === 'material_icons_classic' ) {
 					?>
 					<i class="material-icons search">search</i>
 					<?php
 				} elseif ( $hovercraft_layout_icons === 'font_awesome_version_6' ) {
 					?>
-					<i class="fas fa-search"></i>
+					<i class="fa-solid fa-search"></i>
 					<?php
 				}
 				?>
