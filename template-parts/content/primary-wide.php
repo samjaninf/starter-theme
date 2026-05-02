fd477ed7ce4e8435b9bad552f67826d6e849ed51
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 16:51:18 +0700

    Escape permalink output in templates

--- template-parts/content/primary-wide.php
+++ template-parts/content/primary-wide.php
@@ -20,7 +20,7 @@
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
 			<?php if (is_home()) { ?>
-				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+				<h4><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4>
 				<?php the_excerpt(); ?>
 			<?php } else {
 				the_content();
