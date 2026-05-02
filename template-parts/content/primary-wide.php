c93f689a1cf19eb5445c02ec97c923d3b61889c5
Jesse Nickles <jesse@littlebizzy.com>
2026-05-02 17:58:07 +0700

    Escape primary wide content title output

--- template-parts/content/primary-wide.php
+++ template-parts/content/primary-wide.php
@@ -5,13 +5,13 @@
 	<div id="content-wrapper">
 	
 		<?php if ( is_singular() ) { ?>
-				<?php if ( is_page_template( 'page-templates/template-basic.php' ) || !is_page_template() ) { ?>
-					<h1><?php the_title(); ?></h1>
+				<?php if ( is_page_template( 'page-templates/template-basic.php' ) || ! is_page_template() ) { ?>
+					<h1><?php echo esc_html( get_the_title() ); ?></h1>
 					<?php $url_featured_image = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ), 'large' ); 
-					if ( !empty( $url_featured_image ) && ( !is_singular('product') ) ) { ?>
+					if ( ! empty( $url_featured_image ) && ( ! is_singular( 'product' ) ) ) { ?>
 					<img class="featured-image" src="<?php echo esc_url( $url_featured_image ); ?>" />
 					<?php } ?>
-					<?php if ( 'post' == get_post_type() ) { 
+					<?php if ( get_post_type() === 'post' ) { 
 						the_time(get_option('date_format')); 
 					} ?>
 				<?php } ?>
@@ -20,7 +20,7 @@
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
 			<?php if (is_home()) { ?>
-				<h4><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4>
+				<h4><a href="<?php echo esc_url( get_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a></h4>
 				<?php the_excerpt(); ?>
 			<?php } else {
 				the_content();
