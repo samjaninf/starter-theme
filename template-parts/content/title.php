0e3ea51d66334d57db10246eda35aa7f5798c261
Jesse Nickles <jesse@littlebizzy.com>
2026-05-03 17:53:59 +0700

    Use prefixed hide title meta key in title template

--- template-parts/content/title.php
+++ template-parts/content/title.php
@@ -1,7 +1,12 @@
 <?php 
-$hide_title_status = get_post_meta( get_the_ID(), '_mysite_meta_hide_title', true);
 $post_id = get_the_ID();
-if($hide_title_status === "off" || !metadata_exists( 'post', $post_id, '_mysite_meta_hide_title' ) ) : ?>
+$hide_title_status = get_post_meta( $post_id, '_hovercraft_hide_title', true );
+
+if ( '' === $hide_title_status ) {
+	$hide_title_status = get_post_meta( $post_id, '_mysite_meta_hide_title', true );
+}
+
+if ( $hide_title_status !== 'on' ) : ?>
 	<h1 class="<?php $h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
 	if ( $h1_divider_display === 'everywhere_possible' || $h1_divider_display === 'everywhere_except_heros' ) { echo esc_attr( 'divide' ); } ?>"><?php echo esc_html( get_the_title() ); ?></h1>
 <?php endif; ?>
