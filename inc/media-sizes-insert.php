d90d539a387f9ccdfc368f0d177ffc14346aa03d
Jesse Nickles <jesse@littlebizzy.com>
2026-05-04 02:26:00 +0700

    Clean medium large image label

--- inc/media-sizes-insert.php
+++ inc/media-sizes-insert.php
@@ -3,9 +3,9 @@
 // make medium_large images available to insert into posts
 add_filter( 'image_size_names_choose', 'hovercraft_medium_large_images_insert' );
 function hovercraft_medium_large_images_insert( $sizes ) {
-   return array_merge( $sizes, array(
-      'medium_large' => __( 'Medium Large', 'hovercraft' ),
-   ) );
+    return array_merge( $sizes, array(
+        'medium_large' => esc_html__( 'Medium Large', 'hovercraft' ),
+    ) );
 }
 
 // https://wordpress.stackexchange.com/questions/290259/make-medium-large-images-available-to-insert-into-post
