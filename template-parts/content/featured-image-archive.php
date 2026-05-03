89b1f98f67edeb32e01e0f0c7155472f88d77757
Jesse Nickles <jesse@littlebizzy.com>
2026-05-03 17:32:59 +0700

    Tighten archive featured image output

--- template-parts/content/featured-image-archive.php
+++ template-parts/content/featured-image-archive.php
@@ -8,13 +8,17 @@ if ( empty( $image_id ) || is_singular( 'product' ) ) {
 
 $url_featured_image = wp_get_attachment_image_src( $image_id, 'medium_large' );
 
-if ( empty( $url_featured_image ) ) {
+if ( ! is_array( $url_featured_image ) ) {
     return;
 }
 
 $image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
 $image_width = $url_featured_image[1];
 $image_height = $url_featured_image[2];
+
+if ( ! is_string( $image_alt ) ) {
+    $image_alt = '';
+}
 ?>
-<a href="<?php echo esc_url( get_permalink() ); ?>"><img width="<?php echo esc_attr( $image_width ); ?>" height="<?php echo esc_attr( $image_height ); ?>" class="featured-image-archive" src="<?php echo esc_url( $url_featured_image[0] ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" /></a>
+<a href="<?php echo esc_url( get_permalink() ); ?>"><img width="<?php echo (int) $image_width; ?>" height="<?php echo (int) $image_height; ?>" class="featured-image-archive" src="<?php echo esc_url( $url_featured_image[0] ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" /></a>
 <div class="clear"></div>
