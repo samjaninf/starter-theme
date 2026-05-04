1593767dfd6f9f284860d0af111b63222b7caca1
Jesse Nickles <jesse@littlebizzy.com>
2026-05-04 18:13:20 +0700

    Prefix overlay menu JavaScript globals

--- assets/js/overlay-menu.js
+++ assets/js/overlay-menu.js
@@ -1,11 +1,11 @@
 // open overlay mobile menu
-function openNav() {
-	document.getElementById( 'myNav' ).style.height = '100%';
+function hovercraftOpenNav() {
+	document.getElementById( 'hovercraft-overlay-menu' ).style.height = '100%';
 }
 
 // close overlay mobile menu
-function closeNav() {
-	document.getElementById( 'myNav' ).style.height = '0%';
+function hovercraftCloseNav() {
+	document.getElementById( 'hovercraft-overlay-menu' ).style.height = '0%';
 }
 
 // Ref: ChatGPT
