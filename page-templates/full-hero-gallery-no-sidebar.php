8e46d91f060038ade332bdf09e55cfe3fc130520
Jesse Nickles <jesse@littlebizzy.com>
2026-05-05 00:16:52 +0700

    Use main content helper in full hero gallery template

--- page-templates/full-hero-gallery-no-sidebar.php
+++ page-templates/full-hero-gallery-no-sidebar.php
@@ -11,8 +11,7 @@
 	<?php endif; ?>
 
 
-<?php $hovercraft_homepage_hide_main_checked = get_theme_mod( 'hovercraft_homepage_hide_main' ) ? true : false;
-if ( !is_front_page() || ( is_front_page() && $hovercraft_homepage_hide_main_checked != true ) ) { ?>
+<?php if ( hovercraft_should_show_main_content() ) { ?>
 
 <div id="main">
 <div class="inner">
