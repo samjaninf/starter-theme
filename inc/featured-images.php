85d5adeb7952b877b4e76583e813a5f6dc17c689
Jesse Nickles <jesse@littlebizzy.com>
2026-05-04 14:36:06 +0700

    Guard featured image helper context

--- inc/featured-images.php
+++ inc/featured-images.php
@@ -6,6 +6,11 @@ add_theme_support( 'post-thumbnails' );
 // featured image
 function hovercraft_post_image_large() {
     global $post;
+
+    if ( ! isset( $post ) || ! is_object( $post ) || empty( $post->ID ) ) {
+        return;
+    }
+
     $args = array( 'post_type' => 'attachment', 'post_mime_type' => 'image', 'numberposts' => 1, 'orderby' => 'menu_order', 'order' => 'ASC', 'post_status' => null, 'post_parent' => $post->ID ); 
     $attachments = get_posts($args);
     if ($attachments) {
