2a01a6cbf88f193a03b1dbb449e7a373713ad7a4
Jesse Nickles <jesse@littlebizzy.com>
2026-05-04 16:30:31 +0700

    Use shared hide-title helper in mini hero

--- template-parts/header/header-mini-hero.php
+++ template-parts/header/header-mini-hero.php
@@ -41,11 +41,7 @@
 <div class="inner">
 	<div class="title-wrapper">
 
-		<?php
-		$hide_title_status = get_post_meta( get_the_ID(), '_mysite_meta_hide_title', true );
-		$post_id = get_the_ID();
-		if ( $hide_title_status === 'off' || ! metadata_exists( 'post', $post_id, '_mysite_meta_hide_title' ) ) :
-		?>
+		<?php if ( ! hovercraft_is_title_hidden() ) : ?>
 		<h1 class="mini-hero-title <?php
 			$h1_divider_display = get_theme_mod( 'hovercraft_h1_divider_display', 'none' );
 			if ( $h1_divider_display === 'everywhere_possible' ) {
