a46909975d2e9de09edab593ab048233aad4e95c
Jesse Nickles <jesse@littlebizzy.com>
2026-05-04 15:46:23 +0700

    Bump Hovercraft version constant to 0.3.16

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( ! defined( 'HOVERCRAFT_VERSION' ) ) {
-	define( 'HOVERCRAFT_VERSION', '0.3.15' );
+	define( 'HOVERCRAFT_VERSION', '0.3.16' );
 }
 
 // disable wordpress.org updates for this theme
